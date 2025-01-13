d34dcbb7b12381e4e7407a19f260bc24b53248e2
Ronald Bvirinyangwe <93228943+ronaldbvirinyangwe@users.noreply.github.com>
2025-01-13 11:38:01 +0200

    Add files via upload

---
+++ assets/assets.js
@@ -0,0 +1,102 @@
+import history_icon from './history_icon.png'
+import menu_icon from './menu_icon.png'
+import plus_icon from './plus_icon.png'
+import question_icon from './question_icon.png'
+import setting_icon from './setting_icon.png'
+import bulb_icon from './bulb_icon.png'
+import compass_icon from './compass_icon.png'
+import gallery_icon from './gallery_icon.png'
+import mic_icon from './mic_icon.png'
+import user_icon from './user_icon.png'
+import youtube_icon from './youtube_icon.png'
+import message_icon from './message_icon.png'
+import code_icon from './code_icon.png'
+import send_icon from './send_icon.png'
+import gemini_icon from './gemini_icon.png'
+import facebook from './facebook.png'
+import discord from './discord.png'
+import twitter from './twitter.png'
+import profile from './scales.png'
+import physics from './physics.png'
+import chemistry from './chemistry.png'
+import biology from './biology.png'
+import science from './science.png'
+import agriculture from './agriculture.png'
+import computers from './computers.png'
+import foods from './foods.png'
+import music from './music.png'
+import history from './history.png'
+import art from './art.png'
+import english from './english.png'
+import accounts from './accounts.png'
+import business from './business.png'
+import home_eco from './home_eco.png'
+import geo from './geo.png'
+import heritage from './heritage.png'
+import fareme from './fareme.png'
+import geography from './geography.png'
+import metal from './metal.png'
+import shona from './shona.png'
+import maths from './maths.png'
+import woodwork from './woodwork.png'
+import technical from './technical.png'
+import economics from './economics.png'
+import pe from './pe.png'
+import commerce from './commerce.png'
+import fashion from './fashion.png'
+import learn from './learn.png'
+
+
+export const assets = {
+    history_icon,
+    fashion,
+    fareme,
+    commerce,
+    history,
+    chemistry,
+    biology,
+    physics,
+    menu_icon,
+    plus_icon,
+    question_icon,
+    setting_icon,
+    bulb_icon,
+    compass_icon,
+    gallery_icon,
+    mic_icon,
+    user_icon,
+    youtube_icon,
+    message_icon,
+    code_icon,
+    send_icon,
+    gemini_icon,
+    facebook,
+    twitter,
+    discord,
+    twitter,
+    discord,
+    profile,
+    agriculture,
+    computers,
+    foods,
+    music,
+    art,
+    english,
+    accounts,
+    business,
+    home_eco,
+    geo,
+    heritage,
+    fareme,
+    geography,
+    metal,
+    shona,
+    maths,
+    woodwork,
+    technical,
+    pe,
+    science,
+    economics,
+    business,
+    learn,
+}
